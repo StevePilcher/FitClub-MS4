cf77c5a45c0239b56211af40bb4a2147fa27ad36
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-07 16:23:04 +0000

    work on checkout view.py with payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import render, reverse, redirect
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51ISLJHGMhoNDWXqEOPqFlNiQVFvRliWGIX7tBXkx8MYuptNpRIg3iP3F4JmTEO7x6rA3S3dSfEZL0QkPeiAfpfnY00IHUiFhXF',
-        'client_secret': 'test secret key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
