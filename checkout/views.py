3142902889da005127d41c235576c16583907493
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-07 15:10:50 +0000

    style card element on checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ISLJHGMhoNDWXqEOPqFlNiQVFvRliWGIX7tBXkx8MYuptNpRIg3iP3F4JmTEO7x6rA3S3dSfEZL0QkPeiAfpfnY00IHUiFhXF',
+        'client_secret': 'test secret key',
     }
 
     return render(request, template, context)
