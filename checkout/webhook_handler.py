f17a10bebc9bd0821dfe46267a6f15284ee6d481
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-08 19:36:05 +0000

    create form caching for payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,9 +14,13 @@ class StripeWH_Handler:
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
-        """ Handle payment_intent.succeeded webhook from Stripe"""
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
-            content=f'Webhook recieved: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_failed(self, event):
