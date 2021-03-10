3bd8844f86d1a24a384f6a5d12262f596444b1f2
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-10 16:30:41 +0000

    corrected AllAuth templates and created test user successfully

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_countries.fields import CountryField
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """ A User profile for containing all \
@@ -30,11 +30,11 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update user profile
+    Create or update the user profile
     """
     if created:
-        UserProfile.ojects.create(user=instance)
-        # Save existing users
-        instance.UserProfile.save()
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    # instance.userprofile.save()
