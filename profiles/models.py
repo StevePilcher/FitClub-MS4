6730f903f3548cde32efc1e95aaeacaede79fffd
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-10 19:15:06 +0000

    setup profile template, forms, models and views

--- profiles/models.py
+++ profiles/models.py
@@ -10,20 +10,21 @@ class UserProfile(models.Model):
         user related info, orders, training, reviews """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_image = models.ImageField(null=True, blank=True)
+    user_image_url = models.URLField(max_length=1024, null=True, blank=True)
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True)
-    default_country = CountryField(
-        blank_label='Country *', null=False, blank=False)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(
-        max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(
         max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(
         max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
-    user_image_url = models.URLField(max_length=1024, null=True, blank=True)
-    user_image = models.ImageField(null=True, blank=True)
+    default_country = CountryField(
+        blank_label='Country', null=False, blank=False)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+
 
     def __str__(self):
         return self.user.username
