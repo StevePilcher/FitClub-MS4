f9318d3a2cca2db474bbb6e41768b7e26a2eab83
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-11 15:40:20 +0000

    update my profile page and checkout autofill

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,7 +15,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'grand_total', 'original_bag',
                        'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
@@ -24,9 +24,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total')
+                    'grand_total',)
 
     ordering = ('-date',)
-
 
 admin.site.register(Order, OrderAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+
 class UserProfile(models.Model):
     """ A User profile for containing all \
         user related info, orders, training, reviews """
@@ -25,7 +26,6 @@ class UserProfile(models.Model):
         blank_label='Country', null=False, blank=False)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
 
-
     def __str__(self):
         return self.user.username
 
@@ -38,4 +38,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
-    # instance.userprofile.save()
+    instance.userprofile.save()
