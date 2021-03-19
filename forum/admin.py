13cb2e0f9e81517b753896beb026d2fcc0af9182
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-19 17:22:21 -0300

    update forum model and troubleshoot migration errors

--- forum/admin.py
+++ forum/admin.py
@@ -3,14 +3,3 @@ from .models import Forum
 
 
 # Register your models here.
-class ForumAdmin(admin.ModelAdmin):
-    list_display = (
-        'topic',
-        'date_time',
-    )
-
-    ordering = ('date_time',)
-
-
-admin.site.register(Forum, ForumAdmin)
-
--- forum/models.py
+++ forum/models.py
@@ -4,16 +4,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Forum(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=False, default=True)
     description = models.CharField(max_length=80)
 
     def __str__(self):
         return str(self.name)
 
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
+    Forum = models.ForeignKey(Forum, related_name='topics', on_delete=models.CASCADE)
+    originator = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics')
-    originator = models.ForeignKey(User, related_name='topics')
 
-class Board(model.Models):
+    def __str__(self):
+        return str(self.subject)
+
+
+class Messages(models.Model):
+    message = models.TextField(max_length=1000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.message)
