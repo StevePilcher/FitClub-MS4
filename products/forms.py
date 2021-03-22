b5f58fb2ffb7172a27407adb05ef317d5750c698
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-22 09:08:58 -0300

    continue to improve review logic

--- products/forms.py
+++ products/forms.py
@@ -3,8 +3,7 @@ from .models import Review
 
 
 class NewReviewForm(forms.ModelForm):
-    message = forms.CharField(max_length=500)
 
     class Meta:
         model = Review
-        fields = ['review_title', 'review_description']
+        fields = ['review_title','review_description']
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .models import Product, Category
+from .models import Product, Category, Review
 from profiles.models import UserProfile
 from .forms import NewReviewForm
 
@@ -42,26 +42,30 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details & reviews """
+    """ A view to show individual
+    product details & enable review
+     if the user has purchased the
+     product before"""
+
     product = get_object_or_404(Product, pk=product_id)
     profile = get_object_or_404(UserProfile, user=request.user)
-    can_review = profile.orders.filter(lineitems__product_id=product_id)
-    print(can_review)
-
+    owns_item = profile.orders.filter(lineitems__product_id=product_id)
+    reviews = Review.objects.filter(product_id=product.id)
+    print(owns_item)
+    print(request.user)
     if request.method == 'POST':
         form = NewReviewForm(request.POST)
         if form.is_valid():
-            review = form.save()
+            review = form.save(commit=False)
             review.product = product
             review.created_by = profile
             review.save()
             messages.success(request, 'Your review has been posted, thanks!')
 
-    print('outer_loop')
     context = {
+
         'product': product,
-        'can_review': can_review,
+        'reviews': reviews,
+        'owns_item': owns_item,
     }
-
     return render(request, 'products/product_detail.html', context)
-
