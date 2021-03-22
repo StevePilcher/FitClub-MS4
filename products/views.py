845db770d25ab246440ab074e2f31d2276add72a
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-22 05:46:18 -0300

    continue work on product reviews, review view not yet functional

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Product, Category
 from profiles.models import UserProfile
+from .forms import NewReviewForm
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -45,12 +48,20 @@ def product_detail(request, product_id):
     can_review = profile.orders.filter(lineitems__product_id=product_id)
     print(can_review)
 
+    if request.method == 'POST':
+        form = NewReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            review.product = product
+            review.created_by = profile
+            review.save()
+            messages.success(request, 'Your review has been posted, thanks!')
+
+    print('outer_loop')
     context = {
         'product': product,
+        'can_review': can_review,
     }
 
     return render(request, 'products/product_detail.html', context)
 
-
-def product_review(request, product_id):
-    """A view to leave a prodcut review"""
