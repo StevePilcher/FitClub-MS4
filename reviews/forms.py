573996c110f76fe68ebf698beb9d031db8f635b5
Steve Pilcher <stevepilcheris@gmail.com>
2021-03-22 03:34:10 -0300

    remove reviews app and migrate to start migrating to products schema

--- reviews/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Review
-
-
-class ProductReviewForm(forms.ModelForm):
-    class Meta:
-        model = Review
-        exclude = ('sku', 'review_user')
