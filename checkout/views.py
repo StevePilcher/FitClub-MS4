7ada2e46030ba4112c338abebd1ba2a7336264c6
Steve Pilcher <stevepilcheris@gmail.com>
2021-04-16 14:26:42 +0000

    QC and validate Checkout app with PEP8, HTML and JS

--- checkout/views.py
+++ checkout/views.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.views.decorators.http import require_POST
-from django.contrib import messages
-from django.conf import settings
-
-from .forms import OrderForm
-from .models import Order, OrderLineItem
-
-from products.models import Product
-from profiles.models import UserProfile
-from profiles.forms import UserProfileForm
-from bag.contexts import bag_contents
-
-import stripe
 import json
+import stripe
+from bag.contexts import bag_contents
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from products.models import Product
+from .models import Order, OrderLineItem
+from .forms import OrderForm
+from django.conf import settings
+from django.contrib import messages
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect, reverse
+from django.shortcuts import HttpResponse, get_object_or_404
 
 
 @require_POST
