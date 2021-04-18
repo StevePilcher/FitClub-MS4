e925e0219669096d48b5059b1df891e178c7d2ff
Steve Pilcher <stevepilcheris@gmail.com>
2021-04-18 14:13:00 +0000

    update AWS static and media files in settings.py

--- iron_paradise/settings.py
+++ iron_paradise/settings.py
@@ -199,8 +199,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override Static and Media Files Storage in Production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 STANDARD_DELIVERY_PERCENTAGE = 10
